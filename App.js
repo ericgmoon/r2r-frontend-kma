ddb84a368b9d04ff56e501e88a83dfca26e11ebf
An Thy <anthytran7@gmail.com>
2021-02-03 22:32:59 +1100

    janky preliminary

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Button from "./src/packages/common/button"
+import Input from "./src/packages/common/input"
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>If you can see this text, the app is working!</Text>
+      <Button/>
+      <Input
+        placeholder="Hello"
+        label="Input here!"
+      />
     </View>
   );
 }
