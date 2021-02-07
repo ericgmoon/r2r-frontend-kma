4fd483cd68c528b79db178b6e7ff84a0d17bc72e
Marcus Lo <m._.l@outlook.com>
2021-02-07 17:34:22 +1100

    navigation

--- src/packages/common/chip/index.js
+++ src/packages/common/chip/index.js
@@ -2,20 +2,19 @@ import React from "react";
 import { Button, ThemeProvider } from "react-native-elements";
 import styles from "./styles";
 
-export default function Chip(props) {
-  const { children, label, value, color, active, ...others } = props;
-
+export default function Chip({ label, value, active, ...rest }) {
   return (
     <ThemeProvider theme={{
       colors: { primary: active ? "#DF6A6A" : `#ECECEC` } }}>
       <Button
+        value={value}
         buttonStyle={styles.button}
         titleStyle={{
           ...styles.buttonTitle,
           color: active ? 'white' : 'black'
         }}
         iconRight
-        {...others}
+        {...rest}
         title={label}
       />
     </ThemeProvider>
--- src/packages/navigation/index.js
+++ src/packages/navigation/index.js
@@ -1,5 +1,12 @@
+import React from 'react'
+import { View } from "react-native";
+
 import Chip from '../common/chip'
 
-export default function Navigation() {
-  return null;
+export default function Navigation({ items, current }) {
+  return (
+    <View>
+      {items.map(item => <Chip label={item.label} value={item.value} />)}
+    </View>
+  )
 }
