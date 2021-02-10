d78a5229a4e617e120d479a9d69d4eb9654e5bf0
Marcus Lo <m._.l@outlook.com>
2021-02-10 15:06:59 +1100

    rm nvagiation from app

--- App.js
+++ App.js
@@ -1,36 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Navigation from "./src/packages/navigation";
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-const example = [
-  {
-    label: "Hair",
-    value: "hair",
-    active: true,
-  },
-  {
-    label: "Skin",
-    value: "skin",
-  },
-  {
-    label: "Clothes",
-    value: "clothes",
-  },
-];
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Navigation items={example} current="skin" />
+    <View>
       <Text>If you can see this text, the app is working!</Text>
     </View>
   );
