9fc88eb8a9f06c5cf674e8fb58669461eb25fb59
lunar6022 <lunar.6022@gmail.com>
2021-02-11 15:15:35 +1100

    KMA-23 Add aliases to packages

---
+++ src/components/common/index.js
@@ -0,0 +1,4 @@
+export { default as Button } from "./button";
+export { default as Chip } from "./chip";
+export { default as Navigation } from "./navigation";
+export { default as Overlay } from "./overlay";
