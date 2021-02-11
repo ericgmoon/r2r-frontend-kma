ebc9b19a01fdd1d48d4ed5ab59338fe00a00363c
George <george.kandamkoalthy@gmail.com>
2021-02-11 11:38:47 +0000

    KMA-11 Added tests

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Overlay from "./src/packages/common/overlay";
+import Button from "./src/packages/common/button";
 
 const styles = StyleSheet.create({
   container: {
@@ -13,7 +15,9 @@ const styles = StyleSheet.create({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>If you can see this text, the app is working!</Text>
+      <Overlay>
+        <Button>Press Me</Button>
+      </Overlay>
     </View>
   );
 }
--- src/packages/common/overlay/test.js
+++ src/packages/common/overlay/test.js
@@ -1,17 +1,73 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import sinon from "sinon";
 import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
+import { Icon } from "react-native-elements";
 import Overlay from "./index";
+import Button from "../button";
 
-test("should trigger onPress", () => {
+test("should trigger onClosePress", () => {
   const callback = sinon.spy();
   const node = shallow(
-    <Overlay className="overlay" onClosePress={callback}>
+    <Overlay className="overlay" isCloseIconVisible onClosePress={callback}>
       button
     </Overlay>
   );
   expect(node.find(".overlay")).toBeDefined();
-  node.find(".overlay").find(".exit").simulate("press");
+  node.find(".overlay").find(Icon).simulate("press");
   sinon.assert.called(callback);
+});
+
+test("should trigger onPress for main button", () => {
+  const callback = sinon.spy();
+  const node = shallow(
+    <Overlay
+      className="overlay"
+      button="Press Me"
+      buttonProps={{ onPress: callback }}
+    >
+      button
+    </Overlay>
+  );
+  expect(node.find(".overlay")).toBeDefined();
+  node.find(".overlay").find(Button).simulate("press");
+  sinon.assert.called(callback);
+});
+
+describe("renders correctly", () => {
+  test("without props", () => {
+    const node = renderer.create(<Overlay />).toJSON();
+    expect(node).toMatchSnapshot();
+  });
+
+  test("with title", () => {
+    const node = renderer.create(<Overlay title="Title" />).toJSON();
+    expect(node).toMatchSnapshot();
+  });
+
+  test("with close icon", () => {
+    const node = renderer.create(<Overlay isCloseIconVisible />).toJSON();
+    expect(node).toMatchSnapshot();
+  });
+
+  test("with custom button", () => {
+    const node = renderer.create(<Overlay button="Press Me" />).toJSON();
+    expect(node).toMatchSnapshot();
+  });
+
+  test("with text", () => {
+    const node = renderer.create(<Overlay>Button</Overlay>).toJSON();
+    expect(node).toMatchSnapshot();
+  });
+
+  test("with button as child", () => {
+    const node = renderer
+      .create(
+        <Overlay>
+          <Button>Press Me</Button>
+        </Overlay>
+      )
+      .toJSON();
+    expect(node).toMatchSnapshot();
+  });
 });
