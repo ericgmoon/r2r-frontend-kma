3912249664f724d74bb9437f33f07aa309609a68
Marcus Lo <m._.l@outlook.com>
2021-02-12 13:53:19 +1100

    fix async storage

--- src/components/common/button/Button.stories.js
+++ src/components/common/button/Button.stories.js
@@ -1,8 +1,11 @@
 import { text } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react-native";
 import React from "react";
 import { Button } from "@r2r/common";
 
 storiesOf("Button", module)
-  .add("Text", () => <Button>{text("Text", "Default")}</Button>)
+  .add("Text", () => (
+    <Button onPress={action("Actioned")}>{text("Text", "Default")}</Button>
+  ))
   .add("Icon Right", () => <Button>😀 😎 👍 💯</Button>);
