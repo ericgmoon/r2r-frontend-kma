1a083881181f8c71cbeeb2a5c4b13566d16a8a85
lunar6022 <lunar.6022@gmail.com>
2021-02-13 21:47:26 +1100

    Lint storybook files, remove unnecessary code in storybook/index.js

--- storybook/index.js
+++ storybook/index.js
@@ -1,21 +1,16 @@
-// if you use expo remove this line
 import React from "react";
-import { AppRegistry, View } from "react-native";
-
+import { View } from "react-native";
 import {
   getStorybookUI,
   configure,
   addDecorator,
 } from "@storybook/react-native";
 import { withKnobs } from "@storybook/addon-knobs";
-
-import requireContext from "require-context.macro";
 import "./rn-addons";
 import styles from "./styles";
-
 import { loadStories } from "./storyLoader";
 
-// enables knobs for all stories
+// Enables knobs for all stories
 addDecorator(withKnobs);
 addDecorator((Story) => (
   <View style={styles.main}>
@@ -23,7 +18,7 @@ addDecorator((Story) => (
   </View>
 ));
 
-// import stories
+// Import stories
 configure(() => {
   loadStories();
 }, module);
@@ -31,9 +26,5 @@ configure(() => {
 const StorybookUIRoot = getStorybookUI({
   asyncStorage: null,
 });
-
-// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
-// If you use Expo you should remove this line.
-AppRegistry.registerComponent("%APP_NAME%", () => StorybookUIRoot);
 
 export default StorybookUIRoot;
