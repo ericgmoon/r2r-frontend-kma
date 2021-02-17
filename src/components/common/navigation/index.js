fc0a7ffb2120a2fc6050fac27d26ff81d8a413a5
lunar6022 <lunar.6022@gmail.com>
2021-02-17 17:44:39 +1100

    KMA-30 Change relative references to alias referencing

--- src/components/common/navigation/index.js
+++ src/components/common/navigation/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View } from "react-native";
-import Chip from "../chip";
+import Chip from "@r2r/common/chip";
 import styles from "./styles";
 
 export default function Navigation({
