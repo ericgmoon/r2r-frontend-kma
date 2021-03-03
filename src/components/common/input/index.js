6f7550751f0671fe223e919f84b4040c4e8d027b
nzh20 <nancyzhao999@gmail.com>
2021-03-03 21:46:01 +1100

    Refactor styles

--- src/components/common/input/index.js
+++ src/components/common/input/index.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { Input as EInput, ThemeProvider } from "react-native-elements";
-import { inputStyle, inputContainerStyle, labelStyle } from "./styles";
+import StyledInput from "./styles";
 
 export default function Input(props) {
   const { placeholder, label, color, icon, style, ...others } = props;
 
   return (
-    <ThemeProvider theme={{ colors: { primary: color || "black" } }}>
-      <EInput
-        inputStyle={inputStyle}
-        inputContainerStyle={{ ...inputContainerStyle, ...style }}
-        placeholder={placeholder}
-        label={label}
-        {...others}
-        labelStyle={labelStyle}
-      />
-    </ThemeProvider>
+    <StyledInput
+      inputContainerStyle={style}
+      placeholder={placeholder}
+      label={label}
+      {...others}
+    />
   );
 }
--- src/components/common/input/styles.js
+++ src/components/common/input/styles.js
@@ -1,21 +1,25 @@
-export const inputStyle = {
-  padding: 12,
-  fontWeight: "bold",
-  fontSize: 32,
-};
+import styled from "styled-components/native";
+import { Input } from "react-native-elements";
 
-// Note: borderWidth doesn't control borderBottom
-export const inputContainerStyle = {
-  borderStyle: "solid",
-  borderColor: "#000",
-  borderRadius: 10,
-  borderWidth: 2,
-  borderBottomWidth: 2,
-  width: "100%",
-};
-
-export const labelStyle = {
-  paddingLeft: 16,
-  paddingBottom: 4,
-  fontSize: 24,
-};
+export default styled(Input).attrs(({ inputContainerStyle }) => ({
+  inputStyle: {
+    padding: 12,
+    fontWeight: "bold",
+    fontSize: 32,
+  },
+  // Note: borderWidth doesn't control borderBottom
+  inputContainerStyle: {
+    borderStyle: "solid",
+    borderColor: "#000",
+    borderRadius: 10,
+    borderWidth: 2,
+    borderBottomWidth: 2,
+    width: "100%",
+    ...inputContainerStyle,
+  },
+  labelStyle: {
+    paddingLeft: 16,
+    paddingBottom: 4,
+    fontSize: 24,
+  },
+}))([]);
